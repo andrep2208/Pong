aa1eafd37bc33da501c66f6dd4dc69ca32757bfa
andrep2208 <40508266+andrep2208@users.noreply.github.com>
2020-03-01 17:25:40 +1300

    adding PongPanel

--- Pong/src/Pong/Pong.java
+++ Pong/src/Pong/Pong.java
@@ -1,23 +1,23 @@
 package Pong;
 import javax.swing.JFrame;
+   
+   public class Pong extends JFrame {
+       
+       private final static String WINDOW_TITLE = "Pong";
+       private final static int WINDOW_WIDTH = 800;
+       private final static int WINDOW_HEIGHT = 600;
+       
+       public Pong() {
+    	   		add(new PongPanel());
+              setTitle(WINDOW_TITLE);
+              setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+              setResizable(false);
+              setVisible(true);
+              setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-public class Pong extends JFrame {
-	private final String tittle = "Pongy";
-	private final int width = 800;
-	private final int height = 600; 
-	public Pong() {
-    	setTitle(tittle);
-    	setSize(width, height);
-    	setResizable(false);
-    	setVisible(true);
-    	setDefaultCloseOperation(EXIT_ON_CLOSE);
-}
-	 
-	public static void main(String[] args) {
-		
-		new Pong();
-		 
-
-	}
- 
-}
+      }
+  
+      public static void main(String[] args) {
+          new Pong();
+      }
+ } 
