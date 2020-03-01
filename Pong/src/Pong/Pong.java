f1c8327e1a2f2eb1d6307ec3f1ee30f80ee4188e
andrep2208 <40508266+andrep2208@users.noreply.github.com>
2020-03-01 17:08:14 +1300

    Future proofing values

--- Pong/src/Pong/Pong.java
+++ Pong/src/Pong/Pong.java
@@ -1,10 +1,23 @@
 package Pong;
+import javax.swing.JFrame;
 
-public class Pong {
-
+public class Pong extends JFrame {
+	private final String tittle = "Pongy";
+	private final int width = 800;
+	private final int height = 600; 
+	public Pong() {
+    	setTitle(tittle);
+    	setSize(width, height);
+    	setResizable(false);
+    	setVisible(true);
+    	setDefaultCloseOperation(EXIT_ON_CLOSE);
+}
+	 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hello world");
-	}
+		
+		new Pong();
+		 
 
+	}
+ 
 }
